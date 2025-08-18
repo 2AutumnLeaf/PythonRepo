b624fe681c53aec8db042454ce076c79e9ca7ce5
2AutumnLeaf <manmeetrehnu1604@gmail.com>
2025-08-18 19:06:29 +0530

    Question to make a list of name of 4 friends and their languages

---
+++ dict_set2.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter you name : ")
+lang = input("Enter you language : ")
+d.update({name : lang})
+
+name = input("Enter you name : ")
+lang = input("Enter you language : ")
+d.update({name : lang})
+
+name = input("Enter you name : ")
+lang = input("Enter you language : ")
+d.update({name : lang})
+
+name = input("Enter you name : ")
+lang = input("Enter you language : ")
+d.update({name : lang})
+
+print(d)
