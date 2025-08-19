0d7a5d7dd80ce371c9b58ff9002ca4da23f33233
2AutumnLeaf <manmeetrehnu1604@gmail.com>
2025-08-19 18:57:10 +0530

    For loops

---
+++ loops2.py
@@ -0,0 +1,14 @@
+#For loops with list
+l = [1 ,2 ,4 ,5 ,7,]
+
+for i in (l):
+    print(i)
+
+#For loops with string
+
+a = "Harry"
+
+for i in (a):
+    print(i)
+
+#Same can be done for tuples too 
