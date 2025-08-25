1cb5c5cc3572efa0559c35201f55ed6f428dfec5
2AutumnLeaf <manmeetrehnu1604@gmail.com>
2025-08-25 17:08:09 +0530

    File IO learning file

--- File_IO.py
+++ File_IO.py
@@ -1,9 +1,29 @@
-# f = open("file.txt")
+# f = open("File.txt")
 # data = f.read()
 # print(data)
 # f.close()
 
 # st = "1AutumnLeaf"
-# f = open("myFile.text","w")
+# f = open("File.txt","w")
 # f.write()
 # f.close()
+
+# f = open("File.txt")
+# lines = f.readlines()             #Returns a list
+# print(lines,type(lines))
+# f.close()
+
+# f= open("File.txt")
+# line1 = f.readline()
+# print(line1, type(line1))
+# line2 = f.readline()
+# print(line2, type(line2))
+# f.close()
+
+f = open("File.txt")
+while not(line == ""):
+    print(line)
+    line = f.readline()
+
+
+f.close()
